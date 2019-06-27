fe31f143d9eae5d470b51907e39b4b934c7f0583
Jürgen Hoffmann <hoffmann@apache.org>
2019-06-27 22:43:36 +0200

    Some cleanup to make sure quit works and removed some unused code

--- src/main/java/io/pivotal/weeklyrotator/TestFunction.java
+++ src/main/java/io/pivotal/weeklyrotator/TestFunction.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
-import org.springframework.shell.standard.ShellOption;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
@@ -118,13 +117,8 @@ public class TestFunction {
         return (files == null || files.isEmpty()) ? null : files.get(0);
     }
 
-    @ShellMethod(value = "Add numbers.", key = "sum")
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    @ShellMethod(value = "Display stuff.", prefix = "-")
-    public String echo(int a, int b, @ShellOption("--third") int c) {
-        return String.format("You said a=%d, b=%d, c=%d", a, b, c);
+    @ShellMethod("End the rotator")
+    public void quit() {
+        System.exit(0);
     }
 }
--- src/main/java/io/pivotal/weeklyrotator/WeeklyRotatorApplication.java
+++ src/main/java/io/pivotal/weeklyrotator/WeeklyRotatorApplication.java
@@ -3,13 +3,16 @@ package io.pivotal.weeklyrotator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.util.StringUtils;
 
 @SpringBootApplication
 @EnableScheduling
 public class WeeklyRotatorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WeeklyRotatorApplication.class, args);
+        String[] disabledCommands = {"--spring.shell.command.quit.enabled=false"};
+        String[] fullArgs = StringUtils.concatenateStringArrays(args, disabledCommands);
+        SpringApplication.run(WeeklyRotatorApplication.class, fullArgs);
 
     }
 
